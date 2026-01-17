338f84e5832c6951fd68e76cf7a0c202472191be
Constanza Mariño <marinoconstanza03@gmail.com>
2026-01-16 21:03:41 -0300

    DOM without functionality

--- week02/loops-practice.js
+++
@@ -1,9 +0,0 @@
-const DAYS = 6;
-const LIMIT = 30;
-let studentReport = [11, 42, 33, 64, 29, 37, 44];
-
-for (i=0; i < studentReport.length; i++){
-    if(studentReport[i] < LIMIT){
-        console.log(studentReport[i]);
-    }
-}
---
+++ week02/scripts/bom.js
@@ -0,0 +1,12 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('______');
+
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
+
+li.textContent = input.value;
+
+li.append(deleteButton);
+
+list.append(li);
